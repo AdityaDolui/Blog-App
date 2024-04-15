31f0a86ae580d656d21539eb01be6530a580ca53
Aditya Dolui <adityadolui99@gmail.com>
2024-04-15 16:14:35 +0530

    new commit

--- src/components/Authlayout.jsx
+++ src/components/Authlayout.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 
-// eslint-disable-next-line react/prop-types
+
 export default function Protected({children, authentication = true}) {
 
     const navigate = useNavigate()
